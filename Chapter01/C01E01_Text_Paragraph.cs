73f328ad0d2d91e7320119148acfa02b99dd8ae3
Lee Dumond <leedumond@gmail.com>
2017-08-31 08:55:32 -0500

    Add project files.

---
+++ Chapter01/C01E01_Text_Paragraph.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using iText.IO.Font;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace Chapter01
+{
+    public class C01E01_Text_Paragraph
+    {
+        private const string DEST = @"C:\Projects2\iTextBuildingBlocks\results\chapter01\text_paragraph.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            // Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+            // Initialize document
+            Document document = new Document(pdf);
+
+            // Add content
+            PdfFont font = PdfFontFactory.CreateFont(FontConstants.TIMES_ROMAN);
+            PdfFont bold = PdfFontFactory.CreateFont(FontConstants.TIMES_BOLD);
+
+            Text title = new Text("The Strange Case of Dr. Jekyll and Mr. Hyde").SetFont(bold);
+            Text author = new Text("Robert Louis Stevenson").SetFont(font);
+            Paragraph p = new Paragraph().Add(title).Add(" by ").Add(author);
+            document.Add(p);
+
+            //Close document
+            document.Close();
+        }
+    }
+}
