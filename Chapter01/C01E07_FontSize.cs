04cc7b5d00a231c33982276af3a6389a7df2c2b8
Lee Dumond <leedumond@gmail.com>
2017-08-31 12:57:32 -0500

    add FontSize example

---
+++ Chapter01/C01E07_FontSize.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace Chapter01
+{
+    public class C01E07_FontSize
+    {
+        private const string DEST = @"C:\Projects2\iTextBuildingBlocks\results\chapter01\font_size.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            // Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+            // Initialize document
+            Document document = new Document(pdf);
+
+            // Add content
+            Text title1 = new Text("The Strange Case of ").SetFontSize(12);
+            Text title2 = new Text("Dr. Jekyll and Mr. Hyde").SetFontSize(16);
+            Text author = new Text("Robert Louis Stevenson");
+
+            Paragraph p = new Paragraph().SetFontSize(8)
+                .Add(title1)
+                .Add(title2)
+                .Add(" by ")
+                .Add(author);
+
+            document.Add(p);
+
+            //Close document
+            document.Close();
+        }
+    }
+}
