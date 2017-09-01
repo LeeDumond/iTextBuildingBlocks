19a88268dba17698fe493cb1488010963f0ed5a7
Lee Dumond <leedumond@gmail.com>
2017-09-01 07:14:49 -0500

    add V2 example

---
+++ Chapter02/C02E06_JekyllHydeV2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Hyphenation;
+using iText.Layout.Properties;
+
+namespace Chapter02
+{
+    public class C02E06_JekyllHydeV2
+    {
+        private const string SRC = @"C:\Projects2\iTextBuildingBlocks\resources\txt\jekyll_hyde.txt";
+        private const string DEST = @"C:\Projects2\iTextBuildingBlocks\results\chapter02\jekyll_hyde_v2.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+            //Initialize document
+            Document document = new Document(pdf);
+            document.SetTextAlignment(TextAlignment.JUSTIFIED).SetHyphenation(new HyphenationConfig("en", "uk", 3, 3));
+
+            StreamReader sr = File.OpenText(SRC);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                document.Add(new Paragraph(line));
+            }
+
+            //Close document
+            document.Close();
+        }
+    }
+}
