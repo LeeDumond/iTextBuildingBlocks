15166d7e0fa5be99050480e5efbaa8059dd296c6
Lee Dumond <leedumond@gmail.com>
2017-09-01 07:29:03 -0500

    add v3 example

---
+++ Chapter02/C02E07_JekyllHydeV3.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using iText.IO.Font;
+using iText.Kernel.Font;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Hyphenation;
+using iText.Layout.Properties;
+
+namespace Chapter02
+{
+    public class C02E06_JekyllHydeV3
+    {
+        private const string SRC = @"C:\Projects2\iTextBuildingBlocks\resources\txt\jekyll_hyde.txt";
+        private const string DEST = @"C:\Projects2\iTextBuildingBlocks\results\chapter02\jekyll_hyde_v3.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+            //Initialize document
+            Document document = new Document(pdf);
+            PdfFont font = PdfFontFactory.CreateFont(FontConstants.TIMES_ROMAN);
+            PdfFont bold = PdfFontFactory.CreateFont(FontConstants.HELVETICA_BOLD);
+
+            document
+                .SetTextAlignment(TextAlignment.JUSTIFIED)
+                .SetHyphenation(new HyphenationConfig("en", "uk", 3, 3))
+                .SetFont(font)
+                .SetFontSize(11);
+
+            StreamReader sr = File.OpenText(SRC);
+            string line;
+            Paragraph p;
+            bool title = true;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                p = new Paragraph(line);
+                p.SetKeepTogether(true);
+
+                if (title)
+                {
+                    p.SetFont(bold).SetFontSize(12);
+                    title = false;
+                }
+                else
+                {
+                    p.SetFirstLineIndent(36);
+                }
+
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    p.SetMarginBottom(12);
+                    title = true;
+                }
+                else
+                {
+                    p.SetMarginBottom(0);
+                }
+
+                document.Add(p);
+            }
+
+            //Close document
+            document.Close();
+        }
+    }
+}
