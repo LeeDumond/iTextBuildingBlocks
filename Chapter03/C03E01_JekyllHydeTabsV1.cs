223242ea42bc04e36bd14b90eebabb6c75579f43
Lee Dumond <leedumond@gmail.com>
2017-09-01 13:52:56 -0500

    add Chapter03 project

---
+++ Chapter03/C03E01_JekyllHydeTabsV1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using iTextBuildingBlocks;
+
+namespace Chapter03
+{
+    public class C03E01_JekyllHydeTabsV1
+    {
+        private static readonly string SRC = $@"{Paths.DataResourcesPath}\jekyll_hyde.csv";
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter03\jekyll_hyde_tabs1.pdf";
+
+        public static void Main(string[] args)
+        {
+            var file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
