0a560e00c9c40f223bb8fe06a2f12353374fc765
Lee Dumond <leedumond@gmail.com>
2017-09-05 10:56:58 -0500

    add image examples

---
+++ Chapter03/C03E08_MaryReillyV1.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using iText.IO.Image;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iTextBuildingBlocks;
+
+namespace Chapter03
+{
+    public class C03E08_MaryReillyV1
+    {
+        private static readonly string MARY = $@"{Paths.ImageResourcesPath}\0117002.jpg";
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter03\mary_reilly_V1.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            Document document = new Document(pdf);
+
+            Paragraph p = new Paragraph(
+                "Mary Reilly is a maid in the household of Dr. Jekyll: ");
+            document.Add(p);
+
+            Image img = new Image(ImageDataFactory.Create(MARY));
+            document.Add(img);
+
+            document.Close();
+        }
+    }
+}
