9c0404938476cc938d006a061620d93f210ead29
Lee Dumond <leedumond@gmail.com>
2017-09-05 14:59:47 -0500

    stub out first div example

---
+++ Chapter04/C04E01_DivExample1.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using iTextBuildingBlocks;
+
+namespace Chapter04
+{
+    public class C04E01_DivExample1
+    {
+        private static readonly string SRC = $@"{Paths.DataResourcesPath}\jekyll_hyde.csv";
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter04\jekyll_hyde_overviewV1.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
