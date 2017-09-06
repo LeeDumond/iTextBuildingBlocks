94c974443aa60b5a8d63bc0b3e651bf6846c8f55
Lee Dumond <leedumond@gmail.com>
2017-09-06 08:55:33 -0500

    add some paragraph and div examples

--- iTextBuildingBlocks/Paths.cs
+++ iTextBuildingBlocks/Paths.cs
@@ -8,7 +8,7 @@ namespace iTextBuildingBlocks
         public static string ResultsPath =>
             Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../results"));
 
-        public static string FontResourcesPath => 
+        public static string FontResourcesPath =>
             Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../resources/fonts"));
 
         public static string TextResourcesPath =>
