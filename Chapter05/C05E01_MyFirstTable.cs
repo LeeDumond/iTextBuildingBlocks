e3609fbff69bbac6ecc220c286fecb184e5b4222
Lee Dumond <leedumond@gmail.com>
2017-09-06 10:56:18 -0500

    stub out Chapter 5 project

---
+++ Chapter05/C05E01_MyFirstTable.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using iTextBuildingBlocks;
+
+namespace Chapter05
+{
+    public class C05E01_MyFirstTable
+    {
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter05\my_first_table.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
