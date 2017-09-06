fe68f4eecba84fc8f4e0669f73e046665417c15a
Lee Dumond <leedumond@gmail.com>
2017-09-06 13:05:32 -0500

    add cell heights example

---
+++ Chapter05/C05E04_ColumnHeights.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Borders;
+using iText.Layout.Element;
+using iTextBuildingBlocks;
+
+namespace Chapter05
+{
+    public class C05E04_ColumnHeights
+    {
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter05\column_heights.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (file.Directory != null && !file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            //Initialize PDF document
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+
+            Paragraph p =
+                new Paragraph("The Strange Case of\nDr. Jekyll\nand\nMr. Hyde")
+                    .SetBorder(new DashedBorder(0.3f));
+
+            // Initialize document
+            Document document = new Document(pdf);
+
+            Table table = new Table(1);
+
+            table.AddCell(p);
+
+            Cell cell = new Cell().SetHeight(16).Add(p);
+            table.AddCell(cell);
+
+            cell = new Cell().SetHeight(144).Add(p);
+            table.AddCell(cell);
+
+            cell = new Cell().Add(p).SetRotationAngle(Math.PI / 6);
+            table.AddCell(cell);
+
+            document.Add(table);
+
+            document.Close();
+        }
+    }
+}
