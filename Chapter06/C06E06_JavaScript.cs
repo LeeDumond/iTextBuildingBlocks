6970d9e011853d8ac2be77ec54e8fded7647ddcd
Lee Dumond <leedumond@gmail.com>
2017-09-08 12:13:40 -0500

    add some more action examples

---
+++ Chapter06/C06E06_JavaScript.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Action;
+using iText.Layout;
+using iText.Layout.Element;
+using iTextBuildingBlocks;
+
+namespace Chapter06
+{
+    public class C06E06_JavaScript
+    {
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter06\jekyll_hyde_javascript.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            Document document = new Document(pdf);
+
+            Link link = new Link("here",
+                PdfAction.CreateJavaScript("app.alert('Boo!');"));
+
+            Paragraph p = new Paragraph()
+                .Add("Click ")
+                .Add(link.SetFontColor(Color.BLUE))
+                .Add(" if you want to be scared.");
+
+            document.Add(p);
+
+            document.Close();
+        }
+    }
+}
---
+++ Chapter06/C06E07_ChainedActions.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using iText.Kernel.Colors;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Action;
+using iText.Layout;
+using iText.Layout.Element;
+using iTextBuildingBlocks;
+
+namespace Chapter06
+{
+    public class C06E07_ChainedActions
+    {
+        private static readonly string DEST = $@"{Paths.ResultsPath}\chapter06\jekyll_hyde_chained.pdf";
+
+        public static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            if (!file.Directory.Exists)
+            {
+                file.Directory.Create();
+            }
+
+            CreatePdf(DEST);
+        }
+
+        private static void CreatePdf(string dest)
+        {
+            PdfDocument pdf = new PdfDocument(new PdfWriter(dest));
+            Document document = new Document(pdf);
+
+            PdfAction action = PdfAction.CreateJavaScript("app.alert('Boo!');");
+            action.Next(PdfAction.CreateGoToR(new FileInfo(C06E04_TOC_GoToNamed.DEST).FullName,1, true ));
+
+            Link link = new Link("here", action);
+
+            Paragraph p = new Paragraph()
+                .Add("Click ")
+                .Add(link.SetFontColor(Color.BLUE))
+                .Add(" if you want to be scared.");
+
+            document.Add(p);
+
+            document.Close();
+        }
+    }
+}
